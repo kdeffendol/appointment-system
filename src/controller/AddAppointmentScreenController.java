20f0585c6d98d732efc09b9ab7e1d0be9b608168
Kelsey Deffendol <kelsey@deffendol.org>
2021-04-08 14:06:57 -0600

    added back button functionality

--- src/controller/AddAppointmentScreenController.java
+++ src/controller/AddAppointmentScreenController.java
@@ -5,9 +5,19 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,7 +25,33 @@ import javafx.fxml.Initializable;
  * @author kelsey
  */
 public class AddAppointmentScreenController implements Initializable {
-
+    
+    @FXML TextField appointmentIdTextField;
+    @FXML TextField titleTextField;
+    @FXML TextField descriptionTextField;
+    @FXML TextField locationTextField;
+    
+    @FXML TextField customerIdTextField;
+    @FXML TextField userIdTextField;
+    @FXML ComboBox contactNameComboBox;
+    
+    
+    /**
+     * 
+     * @param event
+     * @throws IOException 
+     */
+    public void backButtonPushed(ActionEvent event) throws IOException {
+        Parent mainPage = FXMLLoader.load(getClass().getResource("/view/AppointmentTableViewScreen.fxml"));
+        Scene mainScene = new Scene(mainPage);
+        
+        //this line gets the stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(mainScene);
+        window.show();
+    }
+    
     /**
      * Initializes the controller class.
      */
