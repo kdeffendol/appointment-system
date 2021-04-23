ecf737b5aa61bd06181831a7f30aa3a5c90df27b
Kelsey Deffendol <kelsey@deffendol.org>
2021-04-22 21:05:07 -0600

    create getCountryByCountryId method

--- src/repos/CountryRepository.java
+++ src/repos/CountryRepository.java
@@ -5,10 +5,55 @@
  */
 package repos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import model.Country;
+import model.User;
+import utils.DBConnection;
+import utils.DBQuery;
+
 /**
  *
  * @author kelsey
  */
 public class CountryRepository {
+    public static Country getCountryByCountryId(int countryId) throws SQLException, Exception {
+        Connection conn = DBConnection.startConnection(); 
+        
+        String selectStatement = "SELECT * FROM countries WHERE Country_ID = ?";
+        
+        DBQuery.setPreparedStatement(conn, selectStatement); //create prepared statement       
+        PreparedStatement ps = DBQuery.getPreparedStatement();
+        
+        ps.setInt(1, countryId);
+        ps.execute();
+        
+        ResultSet rs = ps.getResultSet(); //get result set
+        
+        if(rs.next() == false) {
+            throw new Exception("CountryId not found");
+        }
+        
+                
+        //mapping
+        int id = rs.getInt("Country_ID");
+        String countryName = rs.getString("Country");
+        LocalDateTime createDate = rs.getTimestamp("Create_Date").toLocalDateTime();
+        String createdBy = rs.getString("Created_By");
+        LocalDateTime lastUpdate = rs.getTimestamp("Last_Update").toLocalDateTime();
+        String lastUpdatedBy = rs.getString("Last_Updated_By");
+        
+        //make user object
+        Country country = new Country(id, countryName, createDate, createdBy, lastUpdate, lastUpdatedBy);
+                       
+        DBConnection.closeConnection(); //close connection
+
+        
+        return country;
+        
+    }
     
 }
