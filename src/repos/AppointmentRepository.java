1d8797f1c472d494c63a06f8b7776a8b215212ba
Kelsey Deffendol <kelsey@deffendol.org>
2021-04-22 20:07:37 -0600

    created getAppointmentbyAppointmentId method

--- src/repos/AppointmentRepository.java
+++ src/repos/AppointmentRepository.java
@@ -10,6 +10,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import model.Appointment;
 import model.User;
 import utils.DBConnection;
 import utils.DBQuery;
@@ -21,7 +22,16 @@ import utils.DBQuery;
 public class AppointmentRepository {
     
     
-    public static User getAppointmentByAppointmentId(int appointmentId) throws SQLException, Exception {
+    
+    
+    /**
+     * 
+     * @param appointmentId
+     * @return the Appointment object created
+     * @throws SQLException
+     * @throws Exception if the appointmentId is not found
+     */
+    public static Appointment getAppointmentByAppointmentId(int appointmentId) throws SQLException, Exception {
         Connection conn = DBConnection.startConnection(); 
         
         String selectStatement = "SELECT * FROM appointment WHERE Appointment_ID = ?";
@@ -40,19 +50,28 @@ public class AppointmentRepository {
         
                 
         //mapping
-        int userId = rs.getInt("User_ID");
-        String user_name = rs.getString("User_Name");
-        String password = rs.getString("Password");
+        int id = rs.getInt("Appointment_ID");
+        String title = rs.getString("Title");
+        String description = rs.getString("Description");
+        String location = rs.getString("Location");
+        String type = rs.getString("Type");
+        LocalDateTime startDate = rs.getTimestamp("Start").toLocalDateTime();
+        LocalDateTime endDate = rs.getTimestamp("End").toLocalDateTime();
         LocalDateTime createDate = rs.getTimestamp("Create_Date").toLocalDateTime();
         String createdBy = rs.getString("Created_By");
         LocalDateTime lastUpdate = rs.getTimestamp("Last_Update").toLocalDateTime();
         String lastUpdatedBy = rs.getString("Last_Updated_By");
+        int customerId = rs.getInt("Customer_ID");
+        int userId = rs.getInt("User_ID");
+        int contactId = rs.getInt("Contact_ID");
+       
         
         //make user object
-        User user = new User(userId, user_name, password, createDate, createdBy, lastUpdate, lastUpdatedBy);
+        Appointment appointment = new Appointment(id, title, description, location, type, startDate, endDate, 
+                createDate, createdBy, lastUpdate, lastUpdatedBy, customerId, userId, contactId);
                        
         DBConnection.closeConnection(); //close connection
         
-        return user;       
+        return appointment;       
     }
 }
